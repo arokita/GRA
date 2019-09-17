dc0de126025afb689224d9df2b46959cd99b0919
arokita <55109774+arokita@users.noreply.github.com>
2019-09-17 20:58:04 +0200

    GUI

---
+++ GraZaDuzoZaMalo/GraLib/ModelGry.cs
@@ -0,0 +1,117 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+namespace GraLib
+
+{
+
+    public partial class ModelGry
+
+    {
+
+        readonly private int wylosowana; // readonly oznacza, że nie można zmienić wartości poza konstruktorem
+
+        public int ZakresOd { get; private set; }
+
+        public int ZakresDo { get; private set; }
+
+        public enum Stan { Trwa, Poddana, Odgadnieta }
+
+        public Stan StanGry { get; private set; }
+
+
+        public List<Ruch> Historia { get; private set; }     // tylko jeden element może być przechowywanym w liście 
+
+
+
+        // konstruktor
+
+        public ModelGry(int zakresOd = 1, int zakresDo = 100)
+
+        {
+
+            ZakresOd = Math.Min(zakresOd, zakresDo);
+
+            ZakresDo = Math.Max(zakresOd, zakresDo);
+
+            Random los = new Random();
+
+            wylosowana = los.Next(ZakresOd, ZakresDo + 1);
+
+            StanGry = Stan.Trwa;
+            Historia = new List<Ruch>();
+
+        }
+
+        // Metody
+
+        public enum Odp { ZaMalo = -1, Trafione = 0, ZaDuzo = 1 };
+
+
+        public Odp Ocena(int propozycja)
+
+        {
+
+            if (propozycja < wylosowana)
+            {
+                //Ruch r = new Ruch (propozycja, Odp.ZaMalo);
+                //Historia.Add(r);                           // te dwie linijki kodu nr 65,66 sa prawie równoważne do 67
+                Historia.Add(new Ruch(propozycja, Odp.ZaMalo));
+                return Odp.ZaMalo;
+            }
+            else if (propozycja > wylosowana)
+            {
+                Historia.Add(new Ruch(propozycja, Odp.ZaDuzo));
+                return Odp.ZaDuzo;
+            }
+            else
+
+            {
+                Historia.Add(new Ruch(propozycja, Odp.Trafione));
+                StanGry = Stan.Odgadnieta;
+
+                return Odp.Trafione;
+
+            }
+
+        }
+
+        public void Poddaj()
+
+        {
+
+            StanGry = Stan.Poddana;
+
+        }
+
+        public int Wylosowana
+
+        {
+
+            get
+
+            {
+
+                if (StanGry == Stan.Poddana || StanGry == Stan.Odgadnieta)
+
+                    return wylosowana;
+
+                else
+
+                    throw new NotSupportedException("nie wolno teraz Ci tej wartości udostępnić");
+
+            }
+
+        }
+
+    }
+
+}
